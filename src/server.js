e048ab193ce5c9ff38c3abc70cc4f265f7804e96
padallain <123842369+padallain@users.noreply.github.com>
2024-10-16 07:03:25 -0400

    Reapply "backend updates to accept deploy"
    This reverts commit 1967d4860e1305f0eff94b1b6a970b744b2670d2.

--- src/server.js
+++ src/server.js
@@ -1,39 +1,33 @@
-const express = require("express");
-const session = require("express-session");
-const morgan = require("morgan");
-const cors = require("cors"); 
-const { connectToDatabase } = require("./db"); // Importa la función de conexión
-const startRoutes = require("./routes/start.routes");
-
+const express = require('express');
+const session = require('express-session');
+const morgan = require('morgan');
+const cors = require('cors');
+const { connectToDatabase } = require('./db'); // Importa la función de conexión
+const startRoutes = require('./routes/start.routes');
 const app = express();
 
 // Aplica el middleware cors a todas las rutas
 app.use(cors());
-
-app.use(morgan("dev"));
-
+app.use(morgan('dev'));
 app.use(session({
-    secret: "your_secret_key",
+    secret: process.env.SECRET_KEY || 'your_secret_key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
+    cookie: { secure: false },
 }));
-
 app.use(express.json());
-app.use("/", startRoutes);
+app.use('/', startRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 connectToDatabase()
-  .then(() => {
-    console.log('Connected to MongoDB');
-
-    // Ahora puedes iniciar el servidor Express
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Ahora puedes iniciar el servidor Express
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
     });
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
